2d6ed3f93bf0e03a3f30a28e6b9fc922cf782e32
Sudhir Shrestha <shrestha201@gmail.com>
2018-03-01 16:09:00 +1100

    Avoid useless and coorelated features

--- goodfeature.py
+++ goodfeature.py
@@ -19,3 +19,12 @@ plt.show()
 # In the diagram the probability of a dog to be a Lab or a Greyhound can be distinguished if the feature 
 # are not close enough but if the feature are close the probability is not perfect. Thats why in Machine Learning
 # we always need multiple features to contribute to the probability accuracy or prediction accuracy.
+
+# AVOID using useless features
+# Features such as dog eye color doesn't co-relate much with the breed type,
+# such feature can hurt classifier accuracy even though these type of feature may appear useful
+
+# Always use independent features
+# Independent features such as height and color tells different types of information
+# whereas features such as height in cm and height in inches are coorelated but usually classifier aren't smart enough 
+# to realize that these features are coorelated and may treat them different and double count resulting in reduction of classifier accuracy.
